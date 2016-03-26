c3889f9f0f99fb64584d56e430a069363af03f1e
David Hatanian <david@hatanian.com>
2016-03-26 09:43:42 +0000

    Extract headers and footers, inject username in templates

--- src/main/java/com/ticketmagpie/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/ticketmagpie/infrastructure/security/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package com.ticketmagpie.infrastructure.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 @Configuration
 @EnableWebSecurity
@@ -32,5 +34,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth
         .authenticationProvider(ticketMagPieAuthenticationProvider);
+  }
+
+  @Bean
+  public SpringSecurityDialect securityDialect() {
+    return new SpringSecurityDialect();
   }
 }
